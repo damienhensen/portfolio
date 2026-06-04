fc7b3e09d581c4575e3310fb6020f008c761b19a
Damien <damien.hensen@hva.nl>
2026-06-04 15:08:23 +0200

    feat: add Latest Articles section

---
+++ frontend/app/components/LatestArticles.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="my-12">
+    <h2 class="text-text font-heading mb-4 text-2xl font-bold">
+      Latest Articles
+    </h2>
+
+    <div class="flex flex-col gap-6">
+      <NuxtLink to="/" class="flex flex-col gap-2">
+        <div class="flex flex-col">
+          <span class="font-heading font-medium uppercase">Oct 12, 2025</span>
+          <h3 class="text-text text-lg font-bold">How to do a thing</h3>
+        </div>
+        <p class="line-clamp-2">
+          In this article we're going in depth on how to do certain things, some
+          things are more thingy than other things, so to make sure you
+          understand how to do a thing, keep reading.
+        </p>
+      </NuxtLink>
+      <NuxtLink to="/" class="flex flex-col gap-2">
+        <div class="flex flex-col">
+          <span class="font-heading font-medium uppercase">Oct 12, 2025</span>
+          <h3 class="text-text text-lg font-bold">How to do a thing</h3>
+        </div>
+        <p class="line-clamp-2">
+          In this article we're going in depth on how to do certain things, some
+          things are more thingy than other things, so to make sure you
+          understand how to do a thing, keep reading.
+        </p>
+      </NuxtLink>
+      <NuxtLink to="/" class="flex flex-col gap-2">
+        <div class="flex flex-col">
+          <span class="font-heading font-medium uppercase">Oct 12, 2025</span>
+          <h3 class="text-text text-lg font-bold">How to do a thing</h3>
+        </div>
+        <p class="line-clamp-2">
+          In this article we're going in depth on how to do certain things, some
+          things are more thingy than other things, so to make sure you
+          understand how to do a thing, keep reading.
+        </p>
+      </NuxtLink>
+    </div>
+  </section>
+</template>
--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -3,5 +3,8 @@
   <Facts />
   <FeaturedWork />
   <TechStack />
-  <WorkHistory />
+  <div class="flex gap-10">
+    <WorkHistory class="flex-1" />
+    <LatestArticles class="w-1/3" />
+  </div>
 </template>
