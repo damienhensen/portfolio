e8d39d3468e5840ea1e43cf0290d9ed5dca3caca
Damien <damien.hensen@hva.nl>
2026-06-05 00:42:44 +0200

    refactor: update hero's

--- frontend/app/components/BlogsHero.vue
+++ frontend/app/components/BlogsHero.vue
@@ -1,11 +1,14 @@
 <template>
   <section class="py-12 lg:pt-24">
     <div class="flex flex-col gap-6 lg:w-1/2">
-      <h1
-        class="text-text font-heading text-2xl font-bold md:text-3xl lg:text-4xl"
-      >
-        Damien's Blog
-      </h1>
+      <div class="flex flex-col gap-2">
+        <p class="font-heading text-xs font-medium uppercase">Welcome To</p>
+        <h1
+          class="text-text font-heading text-2xl font-bold md:text-3xl lg:text-4xl"
+        >
+          Damien's Blog
+        </h1>
+      </div>
       <p>
         Notes from projects, experiments, and things I've learned along the way.
       </p>
--- frontend/app/components/ProjectsHero.vue
+++ frontend/app/components/ProjectsHero.vue
@@ -1,11 +1,14 @@
 <template>
   <section class="py-12 lg:pt-24">
     <div class="flex flex-col gap-6 lg:w-1/2">
-      <h1
-        class="text-text font-heading text-2xl font-bold md:text-3xl lg:text-4xl"
-      >
-        Projects
-      </h1>
+      <div class="flex flex-col gap-2">
+        <p class="font-heading text-xs font-medium uppercase">All of my</p>
+        <h1
+          class="text-text font-heading text-2xl font-bold md:text-3xl lg:text-4xl"
+        >
+          Projects
+        </h1>
+      </div>
       <p>
         A collection of personal, academic, and professional projects built to
         learn, solve problems, and gain experience with different technologies.
